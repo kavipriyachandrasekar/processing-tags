c63bdedf5c51f0f4b7c97475c7820e92358b2859
tvrsimhan <raghavtv27@gmail.com>
2023-04-19 01:46:20 +0530

    remove similar

--- check_similar.py
+++ check_similar.py
@@ -1,9 +1,26 @@
 # given two lists of strings, return a list of most similar strings
 # from the second list to the first list
 
-# import difflib
-# import nltk
+import difflib
+import nltk
 from nltk.corpus import wordnet as wn
+
+def similar_within_list(tag_list):
+    # use difflib to get the most similar words in the list
+    most_similar = []
+    for i in range(len(tag_list)):
+        for j in range(i+1, len(tag_list)):
+            if difflib.SequenceMatcher(None, tag_list[i], tag_list[j]).ratio() > 0.8:
+                print(tag_list[i], tag_list[j])
+
+    return most_similar
+
+def find_root(tag_list):
+    # use nltk to get the root of the words in the list
+    root_list = []
+    for tag in tag_list:
+        root_list.append(nltk.stem.WordNetLemmatizer().lemmatize(tag, 'v'))
+    return root_list
 
 def most_similar(list1, list2):
     # use wordnet to get the synsets of the words in the lists
@@ -20,6 +37,10 @@ def most_similar(list1, list2):
     return most_similar
 
 if __name__ == '__main__':
-    list1 = ['hello', 'laptop', 'colorful']
-    list2 = ['hi', 'earth', 'vibrant']
-    print(most_similar(list1, list2))
+    f = open("tags-processed-final.txt", "r")
+    lines = f.readlines()
+    f.close()
+
+    list1 = lines[0].split(',')
+    # list2 = ['hi', 'earth', 'vibrant']
+    print(similar_within_list(list1))
